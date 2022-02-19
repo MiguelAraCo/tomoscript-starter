115599a88f857d32170c1b55e1840f55878e3425
Miguel Aragon <8313806+MiguelAraCo@users.noreply.github.com>
2022-02-19 10:26:59 +0900

    feat: configure Material UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
-import { AppProps } from 'next/app';
+import { createEmotionCache } from '@/utils/emotion/createEmotionCache';
+import { CacheProvider } from '@emotion/react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { darkTheme } from '@/styles/theme/darkTheme';
+import { EmotionCache } from '@emotion/cache';
+import { AppPropsType } from 'next/dist/shared/lib/utils';
+import { NextRouter } from 'next/dist/client/router';
+
 import '@/styles/global.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const clientSideEmotionCache = createEmotionCache();
+
+export type AppProps<PageProps = any> = {
+  pageProps: PageProps;
+  emotionCache?: EmotionCache;
+} & Omit<AppPropsType<NextRouter, PageProps>, 'pageProps'>;
+
+export default function App({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: AppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
